a1e579e53cfe43a72c5b0ba3c1a5b08bb08f7824
John Gallagher <john@synapticmishap.co.uk>
2023-09-17 14:43:21 +0100

    chore: initial commit

---
+++ src/test/CalculatorTest.java
@@ -0,0 +1,70 @@
+package test;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+import main.Calculator;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAddThreeAndTwo() {
+        Calculator calculator = new Calculator();
+        calculator.actionPerformed(eventWithButton(calculator.NumberBtn[3]));
+        calculator.actionPerformed(eventWithButton(calculator.Add_btn));
+        calculator.actionPerformed(eventWithButton(calculator.NumberBtn[2]));
+        calculator.actionPerformed(eventWithButton(calculator.equalBtn));
+        Assert.assertEquals("Failed to add 3 and 2", "5.0", calculator.valueText());
+    }
+
+    @Test
+    public void testSubtractTwoFromThree() {
+        Calculator calculator = new Calculator();
+        calculator.actionPerformed(eventWithButton(calculator.NumberBtn[3]));
+        calculator.actionPerformed(eventWithButton(calculator.Sub_btn));
+        calculator.actionPerformed(eventWithButton(calculator.NumberBtn[2]));
+        calculator.actionPerformed(eventWithButton(calculator.equalBtn));
+        Assert.assertEquals("Failed to subtract 3 from 2", "1.0", calculator.valueText());
+    }
+
+
+    @Test
+    public void testMultiplyThreeAndTwo() {
+        Calculator calculator = new Calculator();
+        calculator.actionPerformed(eventWithButton(calculator.NumberBtn[3]));
+        calculator.actionPerformed(eventWithButton(calculator.Mul_btn));
+        calculator.actionPerformed(eventWithButton(calculator.NumberBtn[2]));
+        calculator.actionPerformed(eventWithButton(calculator.equalBtn));
+        Assert.assertEquals("Failed to multiply 3 and 2", "6.0", calculator.valueText());
+    }
+
+    @Test
+    public void testDivideThreeByTwo() {
+        Calculator calculator = new Calculator();
+        calculator.actionPerformed(eventWithButton(calculator.NumberBtn[3]));
+        calculator.actionPerformed(eventWithButton(calculator.Div_btn));
+        calculator.actionPerformed(eventWithButton(calculator.NumberBtn[2]));
+        calculator.actionPerformed(eventWithButton(calculator.equalBtn));
+        Assert.assertEquals("Failed to divide 3 by 2", "1.5", calculator.valueText());
+    }
+
+    @Test
+    public void testThreeNumbersSameOperator() {
+        Calculator calculator = new Calculator();
+        calculator.actionPerformed(eventWithButton(calculator.NumberBtn[1]));
+        calculator.actionPerformed(eventWithButton(calculator.Add_btn));
+        calculator.actionPerformed(eventWithButton(calculator.NumberBtn[1]));
+        calculator.actionPerformed(eventWithButton(calculator.Add_btn));
+        calculator.actionPerformed(eventWithButton(calculator.NumberBtn[5]));
+        calculator.actionPerformed(eventWithButton(calculator.equalBtn));
+        Assert.assertEquals("Failed to divide 3 by 2", "&.0", calculator.valueText());
+    }
+
+    private static ActionEvent eventWithButton(JButton calculator) {
+        return new ActionEvent(calculator, 0, "");
+    }
+}
